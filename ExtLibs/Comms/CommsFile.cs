4ee513e97bed71f5bc5d5f43caaf84acc95bf265
LukeMike <luca.micheletti.laser@gmail.com>
2014-05-30 22:14:57 +0200

    Update to the latest official version (1.3.5)

--- ExtLibs/Comms/CommsFile.cs
+++ ExtLibs/Comms/CommsFile.cs
@@ -20,17 +20,19 @@ namespace MissionPlanner.Comms
         DateTime lastread = DateTime.MinValue;
         int lastsecond = 0;
 
+        int step = 0;
+
         //void DiscardOutBuffer();
         public void Open(string filename)
         {
-            bps = 6000;
+            bps = 60000;
             PortName = filename;
             BaseStream = File.OpenRead(PortName);
         }
 
         public void Open()
         {
-            bps = 6000;
+            bps = 60000;
             BaseStream = File.OpenRead(PortName);
         }
         public int Read(byte[] buffer, int offset, int count)
@@ -61,7 +63,7 @@ namespace MissionPlanner.Comms
                     break;
                 }
 
-               // System.Threading.Thread.Sleep(1);      
+                System.Threading.Thread.Sleep(1);      
             }
 
             lastread = DateTime.Now;
@@ -73,7 +75,20 @@ namespace MissionPlanner.Comms
                 lastsecond = DateTime.Now.Second;
             }
             currentbps += count;
-            return BaseStream.Read(buffer, offset, count);
+            int ret = BaseStream.Read(buffer, offset, count);
+
+            if (buffer[0] != 254 && offset == 0)
+                return 0;
+            if (buffer[0] == 254 && offset == 1)
+                step = buffer[1] + 5 + 2; // + header + checksum
+
+            step -= ret;
+
+            // read the timestamp
+            if (step == 0)
+                BaseStream.Read(new byte[8], 0, 8);
+
+            return ret;
         }
         //int Read(char[] buffer, int offset, int count);
         public int ReadByte() { return BaseStream.ReadByte(); }
